1d6363c19501c032c5754a67424678b209d7dee9
CoderNoah <113279627+codernoahx@users.noreply.github.com>
2026-02-18 23:41:42 +0530

    codeforces 1703a: yes or yes?

---
+++ Codeforces/Python/1703a-yes-or-yes.py
@@ -0,0 +1,10 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        s = input().upper()
+        # If s is equal to YES print YES else NO
+        print("YES" if s == "YES" else "NO")
+
+
+if __name__ == "__main__":
+    main()
