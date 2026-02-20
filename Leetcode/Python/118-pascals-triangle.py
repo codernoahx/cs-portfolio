82aeb628b02a6a78fc7989b0fcb6a551f9f99788
CoderNoah <113279627+codernoahx@users.noreply.github.com>
2026-02-20 23:56:23 +0530

    fixed comments

--- Leetcode/Python/118-pascals-triangle.py
+++ Leetcode/Python/118-pascals-triangle.py
@@ -7,12 +7,13 @@ class Solution:
         # We created the first row, that's why -1
         for _ in range(numRows - 1):
             # Adding 0 at both ends in temp list, so that we can get 1 at both ends for the next row easily.
-            # As we're calculating the sum of two adjacent elements and appending it in the new list.
+            # We'll calculate the sum of two adjacent elements and append it in the new list.
             temp = [0] + res[-1] + [0]
             # To temporarily store the newly generated list
             row = []
             # Loop till the len of previous row + 1, since the new row will have 1 more element
-            for i in range(len(res[-1]) + 1):  # same as range(len(temp) - 1):
+            for i in range(len(res[-1]) + 1):  # range(len(temp) - 1):
+                # Append the sum of adjacent elements
                 row.append(temp[i] + temp[i + 1])
             # Append the newly generated list in res list
             res.append(row)
