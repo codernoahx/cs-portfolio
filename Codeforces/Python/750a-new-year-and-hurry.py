c809d4abd8fbac1ddf1bcb0116e6bcba94f00644
CoderNoah <113279627+codernoahx@users.noreply.github.com>
2026-02-23 22:47:24 +0530

    fixed comments

--- Codeforces/Python/750a-new-year-and-hurry.py
+++ Codeforces/Python/750a-new-year-and-hurry.py
@@ -1,7 +1,7 @@
 def main():
     n, k = map(int, input().split())
     total_time = 0
-    # Keep on iterating from 1st till nth problem
+    # Keep on iterating from 1st till nth problem, because problems will start from 1 instead of 0
     for i in range(1, n + 1):
         total_time += 5 * i
         # If one of ith problem in the n number of problems, where k time + total time is more than 4hrs (240 mins)
